0418972f475015a8234cff26ab2fff10d32937ce
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-07 01:13:35 +0100

    feat: create skeleton component to indicate loading states

---
+++ src/shared/components/skeleton.tsx
@@ -0,0 +1,14 @@
+// Local Imports
+import { cn } from "../lib/utils/cn";
+
+export const Skeleton = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn("animate-pulse rounded-2xl bg-black/10", className)}
+      {...props}
+    />
+  );
+};
