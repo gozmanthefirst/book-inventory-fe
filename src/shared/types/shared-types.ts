4e0cbd821080259ddde5727399fb6ffc09e48a54
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-17 20:23:09 +0100

    chore: add shared types

---
+++ src/shared/types/shared-types.ts
@@ -0,0 +1,24 @@
+export type ServerActionResponse<T = undefined> = T extends undefined
+  ? {
+      status: "success" | "error" | "info";
+      details: string;
+      data?: never;
+    }
+  : {
+      status: "success" | "error" | "info";
+      details: string;
+      data: T;
+    };
+
+export type SessionUser =
+  | {
+      id: string;
+      createdAt: Date;
+      updatedAt: Date;
+      email: string;
+      emailVerified: boolean;
+      name: string;
+      image?: string | null | undefined;
+      username?: string | null | undefined;
+    }
+  | undefined;
