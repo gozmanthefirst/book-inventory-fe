5c2747d598069b39277ad6adf0ce411060630e6b
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-17 20:27:43 +0100

    chore: remove unnecessary console logs

--- src/app/(signed-in)/search/page.tsx
+++ src/app/(signed-in)/search/page.tsx
@@ -2,15 +2,9 @@
 import { Suspense } from "react";
 
 // Local Imports
-import { getUser } from "@/features/auth/actions/get-user";
 import { Search } from "@/features/search/components/search";
-import { runParallelAction } from "@/shared/lib/utils/parallel-server-action";
 
 const SearchPage = async () => {
-  const [{ data: user }] = await Promise.all([runParallelAction(getUser())]);
-
-  console.log("USER:", user);
-
   return (
     <Suspense>
       <div className="flex flex-1 flex-col py-4">
