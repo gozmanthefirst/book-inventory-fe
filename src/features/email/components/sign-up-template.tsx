870f4e3eb2d2d3e82e03cefbf5a208b8076ffa03
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-23 22:19:19 +0100

    feat: set up resend

---
+++ src/features/email/components/sign-up-template.tsx
@@ -0,0 +1,8 @@
+export const SignUpTemplate = ({ url }: { url: string }) => {
+  return (
+    <div>
+      <h1>Sign up to Book Inventory</h1>
+      <p>Click the link to verify your email: {url}</p>
+    </div>
+  );
+};
