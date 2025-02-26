3da4ceae58a295358829249b68779a086cebe962
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-27 00:41:11 +0100

    feat: fetch data in the 'signed-in' layout and pass it to children pages/components

--- src/app/(signed-in)/layout.tsx
+++ src/app/(signed-in)/layout.tsx
@@ -1,9 +1,15 @@
 // External Imports
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 // Local Imports
 import { getUser } from "@/features/auth/actions/get-user";
+import { getMyBooks } from "@/features/my-books/actions/get-my-books";
 import { BottomNav } from "@/features/navigation/components/bottom-nav";
 import { Container } from "@/shared/components/container";
 import { Header } from "@/shared/components/header";
@@ -14,20 +20,28 @@ interface Props {
 }
 
 const BooksLayout = async ({ children }: Props) => {
-  const { data: user } = await runParallelAction(getUser());
+  const [{ data: myBooks }, { data: user }] = await Promise.all([
+    runParallelAction(getMyBooks()),
+    runParallelAction(getUser()),
+  ]);
 
   if (!user) {
     redirect("/sign-in");
   }
 
+  const queryClient = new QueryClient();
+
+  queryClient.setQueryData(["my-books"], { data: myBooks });
+  queryClient.setQueryData(["user"], { data: user });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <Container className="flex min-h-dvh flex-col">
         <Header />
         <div className="flex-1">{children}</div>
         <BottomNav />
       </Container>
-    </>
+    </HydrationBoundary>
   );
 };
 
