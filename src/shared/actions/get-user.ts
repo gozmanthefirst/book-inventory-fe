dd2aeccf606662d87c7f6540aad70ecc1f229ed4
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-27 00:38:51 +0100

    feat: create server actions for fetching user and session

---
+++ src/shared/actions/get-user.ts
@@ -0,0 +1,39 @@
+"use server";
+
+// External Imports
+import { User } from "better-auth";
+import { headers } from "next/headers";
+
+// Local Imports
+import { auth } from "@/shared/lib/auth/auth";
+import { createParallelAction } from "../lib/utils/parallel-server-action";
+import { ServerActionResponse } from "../types/shared-types";
+
+// get user
+export const getUser = createParallelAction(
+  async (): Promise<ServerActionResponse | ServerActionResponse<User>> => {
+    try {
+      const headersList = await headers();
+
+      const session = await auth.api.getSession({
+        headers: headersList,
+      });
+
+      if (session?.user) {
+        return {
+          status: "success",
+          details: "User gotten!",
+          data: session.user,
+        };
+      } else {
+        return { status: "error", details: "No user available!" };
+      }
+    } catch (error) {
+      console.log(error);
+      return {
+        status: "error",
+        details: `Error fetching user: ${error}`,
+      };
+    }
+  },
+);
