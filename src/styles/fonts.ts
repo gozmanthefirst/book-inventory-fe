559d9533f4eeb48281eac06e32fa7db03e6c1970
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-03-01 13:47:31 +0100

    chore: add Geist to fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,11 @@
 // External Imports
-import { Alegreya, Epilogue } from "next/font/google";
+import { Alegreya, Epilogue, Geist } from "next/font/google";
 
+export const geist = Geist({
+  subsets: ["latin"],
+  display: "swap",
+  style: ["normal"],
+});
 export const alegreya = Alegreya({
   subsets: ["latin"],
   display: "swap",
