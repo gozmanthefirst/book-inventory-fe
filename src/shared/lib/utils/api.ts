107803376597f30d18582e6c81c995a4ba244d8d
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-03-27 21:31:38 +0100

    chore: separate shared util functions from lib functions

--- src/shared/lib/utils/api.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function createErrorResponse(message: string, status: number) {
-  return NextResponse.json({ status: "error", message }, { status });
-}
---
+++ src/shared/utils/handle-api-error.ts
@@ -0,0 +1,75 @@
+import axios, { AxiosError } from "axios";
+
+import { BackendError } from "@/shared/types/shared-types";
+
+type ErrorResponse = {
+  status: "error";
+  errorCode: string;
+  details: string;
+};
+
+type ErrorHandlerOptions = {
+  errorDescription?: string;
+  defaultMessage?: string;
+  errorMapping?: Record<string, string>;
+};
+
+export function handleApiError(
+  error: unknown,
+  options: ErrorHandlerOptions = {},
+): ErrorResponse {
+  const {
+    errorDescription,
+    defaultMessage = "Something went wrong.",
+    errorMapping = {},
+  } = options;
+
+  if (axios.isAxiosError(error)) {
+    const backendError = (error as AxiosError<BackendError>).response?.data;
+    console.error(`${errorDescription}:`, backendError);
+
+    if (backendError) {
+      // Handle specific error codes if provided in errorMapping
+      if (backendError.code && errorMapping[backendError.code]) {
+        return {
+          status: "error",
+          errorCode: backendError.code,
+          details: errorMapping[backendError.code],
+        };
+      }
+
+      // Handle array or string details from backend
+      if (backendError.details) {
+        return {
+          status: "error",
+          errorCode: backendError.code,
+          details: Array.isArray(backendError.details)
+            ? backendError.details[0]
+            : backendError.details,
+          // ? backendError.details.join(", ")
+        };
+      }
+
+      return {
+        status: "error",
+        errorCode: "ERROR",
+        details: defaultMessage,
+      };
+    }
+
+    // Handle network errors
+    return {
+      status: "error",
+      errorCode: "ERROR",
+      details: "Network error. Please check your connection and try again.",
+    };
+  }
+
+  // Handle unexpected errors
+  console.error("Unexpected error:", error);
+  return {
+    status: "error",
+    errorCode: "ERROR",
+    details: "An unexpected error occurred. Please try again later.",
+  };
+}
